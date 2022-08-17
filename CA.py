d2971cc58041c6588a613321b6ede5be48fee1d1
xralphlauren <dsmyronchuk@gmail.com>
2022-08-17 13:14:09 +0300

    refactoring code 17.08.2022

--- CA.py
+++ CA.py
@@ -2,8 +2,8 @@ import xml.etree.ElementTree as ET
 from xml.dom import minidom
 
 
-name = input('Введите полный нейм БС:')
-MRBTS = int(input('Введите MRBTS БС:'))
+user_name = input('Введите полный нейм БС:')
+user_MRBTS = int(input('Введите MRBTS БС:'))
 Cells1800 = input('Введите перечень Сот 1800 (через запятую):')
 Cells2600 = input('Введите перечень Сот 2600 (через запятую):')
 Cells900 = input('Введите перечень Сот 900 (через запятую):')
@@ -19,13 +19,13 @@ if len(Cells900) > 0:
 
 CAREL = {'class': "CAREL"}
 
-strDistName1 = f'PLMN-PLMN/MRBTS-{MRBTS}LNBTS-{MRBTS}LNCEL-{Cells1800}/CAREL-1'
-strDistName2 = f'PLMN-PLMN/MRBTS-{MRBTS}LNBTS-{MRBTS}LNCEL-{Cells2600}/CAREL-1'
+strDistName1 = f'PLMN-PLMN/MRBTS-{user_MRBTS}LNBTS-{user_MRBTS}LNCEL-{Cells1800}/CAREL-1'
+strDistName2 = f'PLMN-PLMN/MRBTS-{user_MRBTS}LNBTS-{user_MRBTS}LNCEL-{Cells2600}/CAREL-1'
 
 
-def main():
-    new = ET.Element('raml', version='2.0', xmlns='raml20.xsd')
-    cm_data = ET.SubElement(new, 'cmData', xmlns="", type='plan', scope='changes', name=name)
+def create_xml():
+    xml = ET.Element('raml', version='2.0', xmlns='raml20.xsd')
+    cm_data = ET.SubElement(xml, 'cmData', xmlns="", type='plan', scope='changes', name=user_name)
     header = ET.SubElement(cm_data, 'header')
     log = ET.SubElement(header, 'log', dateTime='29.09.2021', action="created", appInfo="PlanExporter")
     log.text = 'InternalValues are used'
@@ -45,13 +45,13 @@ def main():
                     scell_prio = '1'
 
                 managed_object = ET.SubElement(cm_data, 'managedObject', CAREL, version="xL20B_2003_002", distName=
-                                               f'PLMN-PLMN/MRBTS-{MRBTS}/LNBTS-{MRBTS}/LNCEL-{Cells1800[x]}'
+                                               f'PLMN-PLMN/MRBTS-{user_MRBTS}/LNBTS-{user_MRBTS}/LNCEL-{Cells1800[x]}'
                                                f'/CAREL-{n_carel}', operation="create")
                 defaults = ET.SubElement(managed_object, 'defaults', name="System")
                 p1 = ET.SubElement(managed_object, 'p', name="lcrId")
                 p1.text = str(Cells2600[x1])
                 p1 = ET.SubElement(managed_object, 'p', name="lnBtsId")
-                p1.text = str(MRBTS)
+                p1.text = str(user_MRBTS)
                 p1 = ET.SubElement(managed_object, 'p', name="maxNumOfSuppMimoLayer")
                 p1.text = '2'
                 p1 = ET.SubElement(managed_object, 'p', name="pcellSwapAllowed")
@@ -74,13 +74,13 @@ def main():
                     scell_prio = '1'
 
                 managed_object = ET.SubElement(cm_data, 'managedObject', CAREL, version="xL20B_2003_002", distName=
-                                               f'PLMN-PLMN/MRBTS-{MRBTS}/LNBTS-{MRBTS}/LNCEL-{Cells2600[x]}'
+                                               f'PLMN-PLMN/MRBTS-{user_MRBTS}/LNBTS-{user_MRBTS}/LNCEL-{Cells2600[x]}'
                                                f'/CAREL-{n_carel}', ooperation="create")
                 defaults = ET.SubElement(managed_object, 'defaults', name="System")
                 p1 = ET.SubElement(managed_object, 'p', name="lcrId")
                 p1.text = str(Cells1800[x1])
                 p1 = ET.SubElement(managed_object, 'p', name="lnBtsId")
-                p1.text = str(MRBTS)
+                p1.text = str(user_MRBTS)
                 p1 = ET.SubElement(managed_object, 'p', name="maxNumOfSuppMimoLayer")
                 p1.text = '2'
                 p1 = ET.SubElement(managed_object, 'p', name="pcellSwapAllowed")
@@ -110,13 +110,13 @@ def main():
                     scell_prio = '1'
 
                 managed_object = ET.SubElement(cm_data, 'managedObject', CAREL, version="xL20B_2003_002", distName=
-                                               f'PLMN-PLMN/MRBTS-{MRBTS}/LNBTS-{MRBTS}/LNCEL-{Cells900[x2]}/CAREL-'
-                                               f'{n_carel}', operation="create")
+                                               f'PLMN-PLMN/MRBTS-{user_MRBTS}/LNBTS-{user_MRBTS}/LNCEL-{Cells900[x2]}'
+                                               f'/CAREL-{n_carel}', operation="create")
                 defaults = ET.SubElement(managed_object, 'defaults', name="System")
                 p1 = ET.SubElement(managed_object, 'p', name="lcrId")
                 p1.text = str(Cells1800[x])
                 p1 = ET.SubElement(managed_object, 'p', name="lnBtsId")
-                p1.text = str(MRBTS)
+                p1.text = str(user_MRBTS)
                 p1 = ET.SubElement(managed_object, 'p', name="maxNumOfSuppMimoLayer")
                 p1.text = '2'
                 p1 = ET.SubElement(managed_object, 'p', name="pcellSwapAllowed")
@@ -140,13 +140,13 @@ def main():
                     scell_prio = '1'
 
                 managed_object = ET.SubElement(cm_data, 'managedObject', CAREL, version="xL20B_2003_002", distName=
-                                               f'PLMN-PLMN/MRBTS-{MRBTS}/LNBTS-{MRBTS}/LNCEL-{Cells1800[x]}/CAREL-'
-                                               f'{n_carel}', operation="create")
+                                               f'PLMN-PLMN/MRBTS-{user_MRBTS}/LNBTS-{user_MRBTS}/LNCEL-{Cells1800[x]}'
+                                               f'/CAREL-{n_carel}', operation="create")
                 defaults = ET.SubElement(managed_object, 'defaults', name="System")
                 p1 = ET.SubElement(managed_object, 'p', name="lcrId")
                 p1.text = str(Cells900[x2])
                 p1 = ET.SubElement(managed_object, 'p', name="lnBtsId")
-                p1.text = str(MRBTS)
+                p1.text = str(user_MRBTS)
                 p1 = ET.SubElement(managed_object, 'p', name="maxNumOfSuppMimoLayer")
                 p1.text = '2'
                 p1 = ET.SubElement(managed_object, 'p', name="pcellSwapAllowed")
@@ -160,7 +160,7 @@ def main():
             x += 1
 
     managed_object = ET.SubElement(cm_data, 'managedObject', version="xL20B_2003_002", distName=f'PLMN-PLMN/MRBTS-'
-                                   f'{MRBTS}/LNBTS-{MRBTS}/CADPR-0', id="41429697", operation="create")
+                                   f'{user_MRBTS}/LNBTS-{user_MRBTS}/CADPR-0', id="41429697", operation="create")
     defaults = ET.SubElement(managed_object, 'defaults', name="System")
     p1 = ET.SubElement(managed_object, 'p', name='a3Offset')
     p1.text = '-3'
@@ -199,16 +199,16 @@ def main():
     p1 = ET.SubElement(managed_object, 'p', name='scellPdcchOlLa')
     p1.text = '2'
 
-    save_xml('test.xml', new)
+    save_xml(xml)
 
 
-def save_xml(NSNTEST, xml_code):
+def save_xml(xml_code):
     xml_string = ET.tostring(xml_code).decode(errors='ignore')  # до этого было просто .decode()
 
     xml_prettyxml = minidom.parseString(xml_string).toprettyxml()
 
     xmlname1 = 'C://Python/Carrier Aggregation NSN/CA_'
-    xmlname2 = name
+    xmlname2 = user_name
     xmlname3 = '.xml'
     fullnamexml = xmlname1+xmlname2+xmlname3
 
@@ -217,4 +217,4 @@ def save_xml(NSNTEST, xml_code):
 
 
 if __name__ == '__main__':
-    main()
+    create_xml()
